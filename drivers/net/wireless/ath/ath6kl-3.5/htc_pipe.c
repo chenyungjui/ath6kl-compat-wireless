chenyungjui/ath6kl-compat-wireless

commit 1fa1972d489f7b89c039b7e365a9ceae2dd68362

compat-wireless: update OLCA 3.5 to version 3.5.0.56
Signed-off-by: Ray Chen <raychen@qca.qualcomm.com>

drivers/net/wireless/ath/ath6kl-3.5/htc_pipe.c
@@ -263,7 +263,7 @@ static int htc_issue_packets(struct htc_target *target,
 	u16 payload_len;
 	struct sk_buff *nbuf;
 	struct htc_frame_hdr *htc_hdr;
-	struct htc_packet *packet = NULL;
+	struct htc_packet *packet;
 
 	ath6kl_dbg(ATH6KL_DBG_HTC,
 		   "%s: queue: 0x%lX, pkts %d\n", __func__,
@@ -1291,7 +1291,7 @@ struct sk_buff *rx_sg_to_single_netbuf(struct htc_target *target)
 		goto _failed;
 	}
 
-	new_skb = __dev_alloc_skb(target->rx_sg_total_len_exp, GFP_KERNEL);
+	new_skb = __dev_alloc_skb(target->rx_sg_total_len_exp, GFP_ATOMIC);
 	if (new_skb == NULL) {
 		ath6kl_dbg(ATH6KL_DBG_HTC,
 			"%s: can't allocate %u size netbuf\n",

drivers/net/wireless/ath/ath6kl-3.5/cfg80211.c
@@ -154,6 +154,50 @@ static struct ieee80211_supported_band ath6kl_band_5ghz = {
 	},
 };
 
+/* Max. 3 devices = 1STA + 1P2P-DEVICE + 1P2P-GO|P2P-CLIENT */
+static const struct ieee80211_iface_limit ath6kl_limits_p2p_concurrent3[] = {
+	{
+		.max = 1,
+		.types = BIT(NL80211_IFTYPE_STATION),
+	},
+	{	/* Treat P2P-DEVICE as P2P-CLIENT */
+		.max = 2,
+		.types = BIT(NL80211_IFTYPE_P2P_CLIENT) | 
+				BIT(NL80211_IFTYPE_P2P_GO),
+	},
+};
+
+static const struct ieee80211_iface_combination ath6kl_iface_combinations_p2p_concurrent3[] = {
+	{ 
+		.num_different_channels = 1,
+		.max_interfaces = ATH6KL_VIF_MAX,
+		.limits = ath6kl_limits_p2p_concurrent3,
+		.n_limits = ARRAY_SIZE(ath6kl_limits_p2p_concurrent3),
+	},
+};
+
+/* Max. 4 devices = 1STA + 1P2P-DEVICE + 2P2P-GO|P2P-CLIENT */
+static const struct ieee80211_iface_limit ath6kl_limits_p2p_concurrent4[] = {
+	{
+		.max = 1,
+		.types = BIT(NL80211_IFTYPE_STATION),
+	},
+	{	/* Treat P2P-DEVICE as P2P-CLIENT */
+		.max = 3,
+		.types = BIT(NL80211_IFTYPE_P2P_CLIENT) | 
+				BIT(NL80211_IFTYPE_P2P_GO),
+	},
+};
+
+static const struct ieee80211_iface_combination ath6kl_iface_combinations_p2p_concurrent4[] = {
+	{ 
+		.num_different_channels = 1,
+		.max_interfaces = 4,
+		.limits = ath6kl_limits_p2p_concurrent4,
+		.n_limits = ARRAY_SIZE(ath6kl_limits_p2p_concurrent4),
+	},
+};
+
 #define CCKM_KRK_CIPHER_SUITE 0x004096ff /* use for KRK */
 
 static int ath6kl_set_wpa_version(struct ath6kl_vif *vif,
@@ -1059,8 +1103,9 @@ static int ath6kl_cfg80211_scan(struct wiphy *wiphy, struct net_device *ndev,
 	}
 
 	ret = ath6kl_wmi_startscan_cmd(ar->wmi, vif->fw_vif_idx, WMI_LONG_SCAN,
-				       force_fg_scan, false, 0, 0, n_channels,
-				       channels);
+					force_fg_scan, false, 0, 
+					ATH6KL_FG_SCAN_INTERVAL,
+					n_channels, channels);
 	if (ret)
 		ath6kl_err("wmi_startscan_cmd failed\n");
 	else
@@ -2977,7 +3022,13 @@ static int ath6kl_remain_on_channel(struct wiphy *wiphy,
 		ath6kl_err("busy, couldn't get access\n");
 		return -ERESTARTSYS;
 	}
-
+	
+	if(test_bit(ROC_ONGOING, &vif->flags)){
+		up(&ar->sem);
+		ath6kl_dbg(ATH6KL_DBG_WMI, "%s cancel due to previous command not canceled", __func__);
+		return -EBUSY;
+	}
+	
 	/* TODO: if already pending or ongoing remain-on-channel,
 	 * return -EBUSY */
 	id = ++vif->last_roc_id;
@@ -3104,14 +3155,14 @@ static int ath6kl_mgmt_tx(struct wiphy *wiphy, struct net_device *dev,
 	/* AP mode Power saving processing */
 	if (vif->nw_type == AP_NETWORK) {
 		if (ath6kl_mgmt_powersave_ap(vif, 
-									id, 
-									chan->center_freq, 
-									wait, 
-									buf, 
-									len, 
-									no_cck, 
-									dont_wait_for_ack,
-									&wmi_data_flags)) {
+						id, 
+						chan->center_freq, 
+						wait, 
+						buf, 
+						len, 
+						no_cck, 
+						dont_wait_for_ack,
+						&wmi_data_flags)) {
 			up(&ar->sem);
 			return ret;
 		}
@@ -3348,7 +3399,10 @@ struct ath6kl *ath6kl_core_alloc(struct device *dev)
 	ar->dev = dev;
 
 	if (ar->p2p_concurrent) {
-		ar->vif_max = 4;
+		if (ath6kl_mod_debug_quirks(ar, ATH6KL_MODULE_P2P_MAX_FW_VIF))
+			ar->vif_max = 4;	/* FIXME : get from query. */
+		else
+			ar->vif_max = ATH6KL_VIF_MAX;
 	} else {
 		ar->vif_max = 1;
 	}
@@ -3398,6 +3452,23 @@ int ath6kl_register_ieee80211_hw(struct ath6kl *ar)
 	if (ar->p2p) {
 		wiphy->interface_modes |= BIT(NL80211_IFTYPE_P2P_GO) |
 					  BIT(NL80211_IFTYPE_P2P_CLIENT);
+	}
+
+	/* FIXME : other combinations? */
+	if (ar->p2p_concurrent) {
+		if (ar->vif_max == ATH6KL_VIF_MAX) {
+			wiphy->iface_combinations = 
+					ath6kl_iface_combinations_p2p_concurrent3;
+			wiphy->n_iface_combinations = 
+					ARRAY_SIZE(ath6kl_iface_combinations_p2p_concurrent3);
+		} else if (ar->vif_max == 4) {
+			wiphy->iface_combinations = 
+					ath6kl_iface_combinations_p2p_concurrent4;
+			wiphy->n_iface_combinations = 
+					ARRAY_SIZE(ath6kl_iface_combinations_p2p_concurrent4);
+		} else {
+			WARN_ON(1);
+		}
 	}
 
 	/* max num of ssids that can be probed during scanning */
@@ -3515,11 +3586,11 @@ void ath6kl_deinit_if_data(struct ath6kl_vif *vif)
 #endif
 	
 	for (ctr = 0; ctr < AP_MAX_NUM_STA ; ctr++) {
-		if (!skb_queue_empty(&vif->sta_list[ctr].psq))
-			skb_queue_purge(&vif->sta_list[ctr].psq);
+		if (!ath6kl_ps_queue_empty(&vif->sta_list[ctr].psq_data))
+			ath6kl_ps_queue_purge(&vif->sta_list[ctr].psq_data);
 
-		if (!ath6kl_mgmt_queue_empty(&vif->sta_list[ctr].mgmt_psq))
-			ath6kl_mgmt_queue_purge(&vif->sta_list[ctr].mgmt_psq);
+		if (!ath6kl_ps_queue_empty(&vif->sta_list[ctr].psq_mgmt))
+			ath6kl_ps_queue_purge(&vif->sta_list[ctr].psq_mgmt);
 
 		aggr_module_destroy_conn(vif->sta_list[ctr].aggr_conn_cntxt);	
 	}
@@ -3590,6 +3661,9 @@ struct net_device *ath6kl_interface_add(struct ath6kl *ar, char *name,
 	spin_lock_bh(&ar->list_lock);
 	list_add_tail(&vif->list, &ar->vif_list);
 	spin_unlock_bh(&ar->list_lock);
+
+	/* FIXME : handle return code. */
+	ath6kl_p2p_utils_init_port(vif, type);
 
 	return ndev;
 

drivers/net/wireless/ath/ath6kl-3.5/core.h
@@ -45,7 +45,7 @@
 #define TO_STR(symbol) MAKE_STR(symbol)
 
 /* The script (used for release builds) modifies the following line. */
-#define __BUILD_VERSION_ 3.5.0.9999
+#define __BUILD_VERSION_ 3.5.0.56
 
 #define DRV_VERSION		TO_STR(__BUILD_VERSION_)
 
@@ -87,8 +87,17 @@
 
 #define ATH6KL_DISCONNECT_TIMEOUT 	  3
 
+/* Channel dwell time in fg scan */
+#define ATH6KL_FG_SCAN_INTERVAL           100 /* in msec */
+
 /* includes also the null byte */
 #define ATH6KL_FIRMWARE_MAGIC               "QCA-ATH6KL"
+
+/* Default HT CAP Parameters */
+#define ATH6KL_24GHZ_HT40_DEF_STA_IDX		(0)	/* only STA interface */
+#define ATH6KL_24GHZ_HT40_DEF_WIDTH		(1)	/* HT40 enabled */
+#define ATH6KL_24GHZ_HT40_DEF_SGI		(1)	/* SGI enabled */
+#define ATH6KL_24GHZ_HT40_DEF_INTOLR40		(0)	/* disabled */
 
 enum ath6kl_fw_ie_type {
 	ATH6KL_FW_IE_FW_VERSION = 0,
@@ -309,8 +318,20 @@ struct ath6kl_android_wifi_priv_cmd {
 
 #define MBOX_YIELD_LIMIT 99
 
-#define ATH6KL_PSQ_CHECK_AGE   (1 * 1000)  				/* 1 sec. */
-#define ATH6KL_PSQ_MAX_AGE     (5)					/* 5 cycles */
+/* AP-PS */
+#define ATH6KL_PS_QUEUE_CHECK_AGE	(1 * 1000) 		/* 1 sec. */
+#define ATH6KL_PS_QUEUE_MAX_AGE		(5)			/* 5 cycles */
+#define ATH6KL_PS_QUEUE_NO_AGE		(0)			/* no aging */
+
+#define ATH6KL_PS_QUEUE_MAX_DEPTH	(65535)			/* TODO */
+#define ATH6KL_PS_QUEUE_NO_DEPTH	(0)			/* unlimit */
+
+enum ps_queue_type {
+	PS_QUEUE_TYPE_NONE,
+	PS_QUEUE_TYPE_STA_UNICAST,
+	PS_QUEUE_TYPE_STA_MGMT,
+	PS_QUEUE_TYPE_AP_MULTICAST,
+};
 
 #define ATH6KL_RSN_CAP_NULLCONF		(0xffff)
 
@@ -328,6 +349,7 @@ struct ath6kl_android_wifi_priv_cmd {
 #define ATH6KL_CONF_ENABLE_11N			BIT(2)
 #define ATH6KL_CONF_ENABLE_TX_BURST		BIT(3)
 #define ATH6KL_CONF_SUSPEND_CUTPOWER		BIT(4)
+#define ATH6KL_CONF_ENABLE_FLOWCTRL		BIT(5)
 
 enum wlan_low_pwr_state {
 	WLAN_POWER_STATE_ON,
@@ -454,29 +476,40 @@ struct ath6kl_cookie {
 	struct ath6kl_cookie *arc_list_next;
 };
 
-struct ath6kl_mgmt_buf {
+struct ath6kl_ps_buf_desc {
 	struct list_head list;
+
+	u32 age;
+	size_t len;
+
+	/* For DATA */
+	struct sk_buff *skb;
+
+	/* For MGMT */
 	u32 freq;
 	u32 wait;
 	u32 id;
 	bool no_cck;
 	bool dont_wait_for_ack;
-	u32 age;
-	size_t len;
 	u8 buf[1];
 };
 
-struct ath6kl_mgmt_buf_head {
+struct ath6kl_ps_buf_head {
 	struct list_head list;
 	spinlock_t lock;
-	u32 len;
-};
 
-struct ath6kl_data_buf_cb {
-	/* total 48 bytes */
-	u32 reserved;		/* used by cfg80211? */
-	u32 ps_age;
-	u8 reserved2[40];
+	enum ps_queue_type queue_type;
+	int depth;
+
+	/* TODO : change by debugfs? */
+	u32 age_cycle;
+	u32 max_depth;
+
+	/* stat */
+	u32 enqueued;
+	u32 enqueued_err;	
+	u32 dequeued;
+	u32 aged;
 };
 
 struct ath6kl_sta {
@@ -488,11 +521,11 @@ struct ath6kl_sta {
 	u8 auth;
 	u8 wpa_ie[ATH6KL_MAX_IE];
 	struct ath6kl_vif *vif;
-	spinlock_t lock;			/* ath6kl_sta global lock, psq & mgmt_psq also use it. */
+	spinlock_t lock;	/* ath6kl_sta global lock, psq_data & psq_mgmt also use it. */
 
 	/* AP-PS*/
-	struct sk_buff_head psq;
-	struct ath6kl_mgmt_buf_head mgmt_psq;
+	struct ath6kl_ps_buf_head psq_data;
+	struct ath6kl_ps_buf_head psq_mgmt;
 	struct timer_list psq_age_timer;
 	u8 apsd_info;
 
@@ -661,6 +694,7 @@ enum ath6kl_vif_state {
 	ROC_CANCEL_PEND,
 	DISCONNECT_PEND,
 	PMKLIST_GET_PEND,
+	PORT_STATUS_PEND,
 };
 
 struct ath6kl_vif {
@@ -702,8 +736,8 @@ struct ath6kl_vif {
 	struct ath6kl_sta sta_list[AP_MAX_NUM_STA];
 	u8 sta_list_index;
 	struct ath6kl_req_key ap_mode_bkey;
-	struct sk_buff_head mcastpsq;
-	spinlock_t mcastpsq_lock;
+	struct ath6kl_ps_buf_head psq_mcast;
+	spinlock_t psq_mcast_lock;
 	int reconnect_flag;
 	u32 last_roc_id;
 	u32 last_cancel_roc_id;
@@ -721,7 +755,6 @@ struct ath6kl_vif {
 #ifdef ATH6KL_DIAGNOSTIC
 	struct wifi_diag diag;
 #endif    
-        u8 connid;
 	struct p2p_ps_info *p2p_ps_info_ctx;
 };
 
@@ -739,6 +772,7 @@ enum ath6kl_dev_state {
 	SKIP_SCAN,
 	ROAM_TBL_PEND,
 	FIRST_BOOT,
+	USB_REMOTE_WKUP,
 };
 
 enum ath6kl_state {
@@ -747,28 +781,6 @@ enum ath6kl_state {
 	ATH6KL_STATE_DEEPSLEEP,
 	ATH6KL_STATE_CUTPOWER,
 	ATH6KL_STATE_WOW,
-};
-
-struct ath6kl_conn_list
-{
-    struct list_head conn_queue;
-    struct list_head re_queue;
-
-    union
-    {
-        struct
-        {
-            u8 bk_uapsd    : 1;
-            u8 be_uapsd    : 1;
-            u8 vi_uapsd    : 1;
-            u8 vo_uapsd    : 1;
-            u8 ps          : 1;  // 1 means power saved
-            u8 ocs         : 1;  // 1 means off channel
-            u8 res         : 2;
-        };
-        u8 connect_status;
-    };
-    bool previous_can_send;
 };
 
 struct ath6kl {
@@ -953,6 +965,7 @@ struct ath6kl {
 			u8 band;
 			u8 chan_width_40M_supported;
 			u8 short_GI;
+			u8 intolerance_40MHz;
 		} ht_cap_param[IEEE80211_NUM_BANDS];
 	} debug;
 #endif /* CONFIG_ATH6KL_DEBUG */
@@ -963,7 +976,7 @@ struct ath6kl {
 	struct wake_lock wake_lock;
 #endif /* CONFIG_HAS_WAKELOCK */
 #endif
-    struct ath6kl_conn_list conn_list[NUM_CONN];
+	struct ath6kl_p2p_flowctrl *p2p_flowctrl_ctx;
 #ifdef CONFIG_HAS_EARLYSUSPEND
     struct early_suspend early_suspend;
 #endif /* CONFIG_HAS_EARLYSUSPEND */
@@ -990,28 +1003,14 @@ static inline u32 ath6kl_get_hi_item_addr(struct ath6kl *ar,
 	return addr;
 }
 
-static inline u32 ath6kl_data_get_age(struct sk_buff *buf)
+static inline u32 ath6kl_ps_queue_get_age(struct ath6kl_ps_buf_desc *ps_buf)
 {
-	struct ath6kl_data_buf_cb *data_buf_cb = (struct ath6kl_data_buf_cb *)(buf->cb);
-
-	return data_buf_cb->ps_age;
+	return ps_buf->age;
 }
 
-static inline void ath6kl_data_set_age(struct sk_buff *buf, u32 age)
+static inline void ath6kl_ps_queue_set_age(struct ath6kl_ps_buf_desc *ps_buf, u32 age)
 {
-	struct ath6kl_data_buf_cb *data_buf_cb = (struct ath6kl_data_buf_cb *)(buf->cb);
-	
-	data_buf_cb->ps_age = age;
-}
-
-static inline u32 ath6kl_mgmt_get_age(struct ath6kl_mgmt_buf *buf)
-{
-	return buf->age;
-}
-
-static inline void ath6kl_mgmt_set_age(struct ath6kl_mgmt_buf *buf, u32 age)
-{
-	buf->age = age;
+	ps_buf->age = age;
 }
 
 int ath6kl_configure_target(struct ath6kl *ar);
@@ -1047,9 +1046,6 @@ bool ath6kl_mgmt_powersave_ap(struct ath6kl_vif *vif,
                                      bool dont_wait_for_ack,
                                      u32 *flags);
 int ath6kl_data_tx(struct sk_buff *skb, struct net_device *dev);
-int ath6kl_conn_list_init(struct ath6kl *ar);
-void ath6kl_tx_scheduler(struct ath6kl_vif *vif);
-void ath6kl_flowctrl_change(struct ath6kl_vif *vif);
 
 struct aggr_info *aggr_init(struct ath6kl_vif *vif);
 struct aggr_conn_info *aggr_init_conn(struct ath6kl_vif *vif);
@@ -1113,21 +1109,34 @@ void ath6kl_check_wow_status(struct ath6kl *ar);
 void ath6kl_htc_pipe_attach(struct ath6kl *ar);
 void ath6kl_htc_mbox_attach(struct ath6kl *ar);
 
-void ath6kl_mgmt_queue_init(struct ath6kl_mgmt_buf_head *psq);
-void ath6kl_mgmt_queue_purge(struct ath6kl_mgmt_buf_head *psq);
-int ath6kl_mgmt_queue_empty(struct ath6kl_mgmt_buf_head *psq);
-int ath6kl_mgmt_queue_tail(struct ath6kl_mgmt_buf_head *psq, 
-								const u8 *buf, 
-								u16 len, 
-								u32 id, 
-								u32 freq, 
-								u32 wait, 
-								bool no_cck, 
-								bool dont_wait_for_ack);
-struct ath6kl_mgmt_buf *ath6kl_mgmt_dequeue_head(struct ath6kl_mgmt_buf_head *psq);
-void ath6kl_psq_age_handler(unsigned long ptr);
-void ath6kl_psq_age_start(struct ath6kl_sta *conn);
-void ath6kl_psq_age_stop(struct ath6kl_sta *conn);
+void ath6kl_ps_queue_init(struct ath6kl_ps_buf_head *psq,
+			enum ps_queue_type queue_type,
+			u32 age_cycle,
+			u32 max_depth);
+void ath6kl_ps_queue_purge(struct ath6kl_ps_buf_head *psq);
+int ath6kl_ps_queue_empty(struct ath6kl_ps_buf_head *psq);
+int ath6kl_ps_queue_depth(struct ath6kl_ps_buf_head *psq);
+void ath6kl_ps_queue_stat(struct ath6kl_ps_buf_head *psq, 
+			 int *depth, 
+			 u32 *enqueued, 
+			 u32 *enqueued_err, 
+			 u32 *dequeued,
+			 u32 *aged);
+struct ath6kl_ps_buf_desc *ath6kl_ps_queue_dequeue(struct ath6kl_ps_buf_head *psq);
+int ath6kl_ps_queue_enqueue_mgmt(struct ath6kl_ps_buf_head *psq, 
+				const u8 *buf, 
+				u16 len, 
+				u32 id, 
+				u32 freq, 
+				u32 wait, 
+				bool no_cck, 
+				bool dont_wait_for_ack);
+int ath6kl_ps_queue_enqueue_data(struct ath6kl_ps_buf_head *psq, 
+				struct sk_buff *skb);
+void ath6kl_ps_queue_age_handler(unsigned long ptr);
+void ath6kl_ps_queue_age_start(struct ath6kl_sta *conn);
+void ath6kl_ps_queue_age_stop(struct ath6kl_sta *conn);
+
 #ifdef CONFIG_ANDROID
 void ath6kl_sdio_init_msm(void);
 void ath6kl_sdio_exit_msm(void);

drivers/net/wireless/ath/ath6kl-3.5/debug.c
@@ -89,7 +89,7 @@ static const struct ath6kl_diag_reg_info diag_reg[] = {
 	{ 0x29800, 0x2B210, "Base Band" },
 	{ 0x1C000, 0x1C748, "Analog" },
 	{ 0x5000, 0x5160, "WLAN RTC" },
-	{ 0x18000, 0x18170, "GPIO" },
+	{ 0x14000, 0x14170, "GPIO" },
 };
 
 void ath6kl_dump_registers(struct ath6kl_device *dev,
@@ -463,9 +463,13 @@ static ssize_t read_file_tgt_stats(struct file *file, char __user *user_buf,
 	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
 			 "Bcast packets", tgt_stats->tx_bcast_pkt);
 	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
+			 "Mcast packets", tgt_stats->tx_mcast_pkt);
+	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
 			 "Ucast byte", tgt_stats->tx_ucast_byte);
 	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
 			 "Bcast byte", tgt_stats->tx_bcast_byte);
+	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
+			 "Mcast byte", tgt_stats->tx_mcast_byte);
 	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
 			 "Rts success cnt", tgt_stats->tx_rts_success_cnt);
 	for (i = 0; i < 4; i++)
@@ -498,9 +502,13 @@ static ssize_t read_file_tgt_stats(struct file *file, char __user *user_buf,
 	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
 			 "Bcast packets", tgt_stats->rx_bcast_pkt);
 	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
+			 "Mcast packets", tgt_stats->rx_mcast_pkt);
+	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
 			 "Ucast byte", tgt_stats->rx_ucast_byte);
 	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
 			 "Bcast byte", tgt_stats->rx_bcast_byte);
+	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
+			 "Mcast byte", tgt_stats->rx_mcast_byte);
 	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
 			 "Fragmented pkt", tgt_stats->rx_frgment_pkt);
 	len += scnprintf(buf + len, buf_len - len, "%20s %10llu\n",
@@ -534,6 +542,19 @@ static ssize_t read_file_tgt_stats(struct file *file, char __user *user_buf,
 			 "Num disconnects", tgt_stats->cs_discon_cnt);
 	len += scnprintf(buf + len, buf_len - len, "%20s %10d\n",
 			 "Beacon avg rssi", tgt_stats->cs_ave_beacon_rssi);
+
+	len += scnprintf(buf + len, buf_len - len, "%25s\n",
+			 "Wow stats");
+	len += scnprintf(buf + len, buf_len - len, "%25s\n",
+			 "=================");
+	len += scnprintf(buf + len, buf_len - len, "%20s %10u\n",
+			 "Wow pkt dropped", tgt_stats->wow_pkt_dropped);
+	len += scnprintf(buf + len, buf_len - len, "%20s %10u\n",
+			 "Wow evt discarded", tgt_stats->wow_evt_discarded);
+	len += scnprintf(buf + len, buf_len - len, "%20s %10u\n",
+			 "Wow host pkt wakeup", tgt_stats->wow_host_pkt_wakeups);
+	len += scnprintf(buf + len, buf_len - len, "%20s %10u\n",
+			 "Wow host evt wakeups", tgt_stats->wow_host_evt_wakeups);
 
 	if (len > buf_len)
 		len = buf_len;
@@ -925,6 +946,25 @@ static ssize_t ath6kl_lrssi_roam_read(struct file *file,
 static const struct file_operations fops_lrssi_roam_threshold = {
 	.read = ath6kl_lrssi_roam_read,
 	.write = ath6kl_lrssi_roam_write,
+	.open = ath6kl_debugfs_open,
+	.owner = THIS_MODULE,
+	.llseek = default_llseek,
+};
+
+static ssize_t ath6kl_driver_version_read(struct file *file,
+				      char __user *user_buf,
+				      size_t count, loff_t *ppos)
+{
+	char buf[32];
+	unsigned int len;
+
+	len = snprintf(buf, sizeof(buf), "%s\n", DRV_VERSION);
+
+	return simple_read_from_buffer(user_buf, count, ppos, buf, len);
+}
+
+static const struct file_operations fops_driver_version = {
+	.read = ath6kl_driver_version_read,
 	.open = ath6kl_debugfs_open,
 	.owner = THIS_MODULE,
 	.llseek = default_llseek,
@@ -2159,6 +2199,12 @@ static int ath6kl_parse_ht_cap_params(const char __user *user_buf, size_t count,
 	sscanf(p, "%d", &value);
 	htCapParam->short_GI = (u8) value;
 
+	SEEK_SPACE;
+	SKIP_SPACE;
+
+	sscanf(p, "%d", &value);
+	htCapParam->intolerance_40MHz = (u8) value;
+
 	return 0;
 }
 
@@ -2169,7 +2215,7 @@ static ssize_t ath6kl_ht_cap_params_write(struct file *file,
 {
 	struct ath6kl *ar = file->private_data;
 	struct ht_cap_param tempHtCap;
-	int ret;
+	int i, ret;
 
 	memset(&tempHtCap, 0, sizeof(struct ht_cap_param));
 	ret = ath6kl_parse_ht_cap_params(user_buf, count, &tempHtCap);
@@ -2177,12 +2223,14 @@ static ssize_t ath6kl_ht_cap_params_write(struct file *file,
 	if (ret)
 		return ret;
 
-	/* TBD : Per virutal interface. */
-	if (ath6kl_wmi_set_ht_cap_cmd(ar->wmi,
+	for (i = 0; i < ar->vif_max; i++) {
+		if (ath6kl_wmi_set_ht_cap_cmd(ar->wmi, i,
 			tempHtCap.band, 
 			tempHtCap.chan_width_40M_supported,
-			tempHtCap.short_GI))
-		return -EIO;
+			tempHtCap.short_GI,
+			tempHtCap.intolerance_40MHz))
+			return -EIO;
+	}
 
 	tempHtCap.isConfig = 1;
 	memcpy(&ar->debug.ht_cap_param[tempHtCap.band], &tempHtCap, sizeof(struct ht_cap_param));
@@ -2201,10 +2249,11 @@ static ssize_t ath6kl_ht_cap_params_read(struct file *file,
 
 	for (band = 0; band < A_NUM_BANDS; band++) {
 		if (ar->debug.ht_cap_param[band].isConfig)
-			len += scnprintf(buf + len, sizeof(buf) - len, "%s chan_width_40M_supported: %d, short_GI: %d\n",
+			len += scnprintf(buf + len, sizeof(buf) - len, "%s chan_width_40M_supported: %d, short_GI: %d, intolerance_40MHz: %d\n",
 					(ar->debug.ht_cap_param[band].band == A_BAND_24GHZ ? "24GHZ" : "5GHZ"),
 					ar->debug.ht_cap_param[band].chan_width_40M_supported,
-					ar->debug.ht_cap_param[band].short_GI);
+					ar->debug.ht_cap_param[band].short_GI,
+					ar->debug.ht_cap_param[band].intolerance_40MHz);
 	}
 
 	return simple_read_from_buffer(user_buf, count, ppos, buf, len);
@@ -2932,6 +2981,87 @@ static const struct file_operations fops_txseries_write = {
 	.llseek = default_llseek,
 };
 
+static ssize_t ath6kl_p2p_flowctrl_stat_read(struct file *file, 
+				char __user *user_buf,
+				size_t count, loff_t *ppos)
+{
+	struct ath6kl *ar = file->private_data;
+	static u8 buf[2048];
+	unsigned int len;
+
+	len = ath6kl_p2p_flowctrl_stat(ar, buf, 2048);
+
+	return simple_read_from_buffer(user_buf, count, ppos, buf, len);
+}
+
+
+/* debug fs for P2P Flowctrl Stats. */
+static const struct file_operations fops_p2p_flowctrl_stat = {
+	.read = ath6kl_p2p_flowctrl_stat_read,
+	.open = ath6kl_debugfs_open,
+	.owner = THIS_MODULE,
+	.llseek = default_llseek,
+};
+
+static ssize_t ath6kl_ap_ps_stat_read(struct file *file, 
+				char __user *user_buf,
+				size_t count, loff_t *ppos)
+{
+	struct ath6kl *ar = file->private_data;
+	struct ath6kl_vif *vif;
+	struct ath6kl_sta *conn;
+	static u8 buf[1024];
+	u8 *p;
+	unsigned int len = 0;
+	int i, j, buf_len, depth;
+	u32 enq, enq_err, deq, age;
+
+	p = buf;
+	buf_len = sizeof(buf);
+	for (i = 0; i < ar->vif_max; i++) {
+		vif = ath6kl_get_vif_by_index(ar, i);
+		if ((vif) &&
+			(vif->nw_type == AP_NETWORK)) {
+
+			ath6kl_ps_queue_stat(&vif->psq_mcast, &depth, &enq, &enq_err, &deq, &age);
+			len += scnprintf(p + len, buf_len - len, "VIF[%d] = psq_mcast %d/%d/%d/%d/%d\n",
+					vif->fw_vif_idx,
+					depth, enq, enq_err, deq, age);
+
+			for (j = 0; j < AP_MAX_NUM_STA; j ++) {
+				conn = &vif->sta_list[j];
+
+				len += scnprintf(p + len, buf_len - len,
+					" STA - %02x:%02x:%02x:%02x:%02x:%02x aid %02d apsd %d",
+					conn->mac[0], conn->mac[1], conn->mac[2],
+					conn->mac[3], conn->mac[4], conn->mac[5],
+					conn->aid,
+					conn->apsd_info);
+
+				ath6kl_ps_queue_stat(&conn->psq_data, &depth, &enq, &enq_err, &deq, &age);
+				len += scnprintf(p + len, buf_len - len,
+					" psq_data %d/%d/%d/%d/%d",
+					depth, enq, enq_err, deq, age);
+
+				ath6kl_ps_queue_stat(&conn->psq_mgmt, &depth, &enq, &enq_err, &deq, &age);
+				len += scnprintf(p + len, buf_len - len,
+					" psq_mgmt %d/%d/%d/%d/%d \n",
+					depth, enq, enq_err, deq, age);
+			}
+		}
+	}
+
+	return simple_read_from_buffer(user_buf, count, ppos, buf, len);
+}
+
+/* debug fs for AP-PS Stats. */
+static const struct file_operations fops_ap_ps_stat = {
+	.read = ath6kl_ap_ps_stat_read,
+	.open = ath6kl_debugfs_open,
+	.owner = THIS_MODULE,
+	.llseek = default_llseek,
+};
+
 int ath6kl_debug_init(struct ath6kl *ar)
 {
 	ar->debug.fwlog_buf.buf = vmalloc(ATH6KL_FWLOG_SIZE);
@@ -2983,6 +3113,9 @@ int ath6kl_debug_init(struct ath6kl *ar)
 
 	debugfs_create_file("lrssi_roam_threshold", S_IRUSR | S_IWUSR,
 			    ar->debugfs_phy, ar, &fops_lrssi_roam_threshold);
+
+	debugfs_create_file("driver_version", S_IRUSR | S_IWUSR,
+			    ar->debugfs_phy, ar, &fops_driver_version);
 
 	debugfs_create_file("reg_write", S_IRUSR | S_IWUSR,
 			    ar->debugfs_phy, ar, &fops_diag_reg_write);
@@ -3073,6 +3206,12 @@ int ath6kl_debug_init(struct ath6kl *ar)
 
 	debugfs_create_file("txrate_sereies", S_IWUSR,
 			    ar->debugfs_phy, ar, &fops_txseries_write);
+
+	debugfs_create_file("p2p_flowctrl_stat", S_IRUSR,
+			    ar->debugfs_phy, ar, &fops_p2p_flowctrl_stat);
+
+	debugfs_create_file("ap_ps_stat", S_IRUSR,
+			    ar->debugfs_phy, ar, &fops_ap_ps_stat);
 
 	return 0;
 }

drivers/net/wireless/ath/ath6kl-3.5/debug.h
@@ -28,6 +28,10 @@ enum ATH6KL_MODULE_QUIRKS {
 	ATH6KL_MODULES_ANI_ENABLE       = BIT(5), /* anti-noise-immunity */
 	ATH6KL_MODULE_ENABLE_EPPING		= BIT(6), /* enable endpoing loopback */
 	ATH6KL_MODULE_DISABLE_5G		= BIT(7), /* disable 5G support */
+	ATH6KL_MODULE_P2P_MAX_FW_VIF	= BIT(8), /* enable max. fw vif */
+	ATH6KL_MODULE_P2P_FLOWCTRL	= BIT(9), /* enable p2p flowctrl */
+	ATH6KL_MODULE_ENABLE_USB_REMOTE_WKUP= BIT(10), /* enable usb remote wakeup support */
+	ATH6KL_MODULE_DISABLE_2G_HT40	= BIT(11), /* disable 2.4G HT40 in default */
 };
 
 enum ATH6KL_MODULE_P2P {
@@ -63,7 +67,8 @@ enum ATH6K_DEBUG_MASK {
 	ATH6KL_DBG_WLAN_TX_AMSDU = BIT(24),    /* wlan tx a-msdu */
 	ATH6KL_DBG_POWERSAVE 	= BIT(25),    /* power-saving */
 	ATH6KL_DBG_WOWLAN	= BIT(26),    /* wowlan tracing */
-        ATH6KL_DBG_RTT          = BIT(27),
+	ATH6KL_DBG_RTT          = BIT(27),
+	ATH6KL_DBG_FLOWCTRL     = BIT(28),    /* P2P flowctrl */
 	ATH6KL_DBG_ANY	        = 0xffffffff  /* enable all logs */
 };
 

drivers/net/wireless/ath/ath6kl-3.5/hif-ops.h
@@ -237,4 +237,11 @@ static inline void ath6kl_hif_late_resume(struct ath6kl *ar)
 }	
 #endif
 
+static inline int ath6kl_hif_bus_config(struct ath6kl *ar)
+{
+	ath6kl_dbg(ATH6KL_DBG_HIF, "hif buf config\n");
+
+	return ar->hif_ops->bus_config(ar);
+}
+
 #endif

drivers/net/wireless/ath/ath6kl-3.5/hif.h
@@ -281,6 +281,7 @@ struct ath6kl_hif_ops {
 	void (*early_suspend)(struct ath6kl *ar);
 	void (*late_resume)(struct ath6kl *ar);
 #endif
+	int (*bus_config)(struct ath6kl *ar);
 
 };
 

drivers/net/wireless/ath/ath6kl-3.5/htcoex.c
@@ -145,7 +145,7 @@ static void htcoex_scan_start(unsigned long arg)
 		goto resche;
 
 	ret = ath6kl_wmi_startscan_cmd(ar->wmi, vif->fw_vif_idx, WMI_LONG_SCAN,
-				       1, false, 0, 0, 0, NULL);
+				       0, false, 0, 0, 0, NULL);
 	if (ret)
 		ath6kl_err("wmi_startscan_cmd failed\n");
 	else {
@@ -272,6 +272,13 @@ struct htcoex *ath6kl_htcoex_init(struct ath6kl_vif *vif)
 	/* Disable by default. */
 	coex->flags &= ~ATH6KL_HTCOEX_FLAGS_ENABLED;
 	coex->scan_interval = ATH6KL_HTCOEX_SCAN_PERIOD;
+
+	/*
+	 * FIXME : No rate roll-back by default. Host driver need 
+	 *         more information to decide if current AP accept 
+	 *         from HT20 to HT40 under AP current support rates 
+	 *         include HT40.
+	 */
 	coex->rate_rollback_interval = ATH6KL_HTCOEX_RATE_ROLLBACK;
 
 	/* Init. periodic scan timer. */

commit 0a20e63d49b8f7a685b0221b638f4e8437a12e8b

compat-wireless: Fix kernel 3.4 compile issues for OLCA 3.5
The change is reverted by commit 1fa1972d mistakenly. So this change
correct the error.

Signed-off-by: Ray Chen <raychen@qca.qualcomm.com>

drivers/net/wireless/ath/ath6kl-3.5/htc_pipe.c
@@ -263,7 +263,7 @@ static int htc_issue_packets(struct htc_target *target,
 	u16 payload_len;
 	struct sk_buff *nbuf;
 	struct htc_frame_hdr *htc_hdr;
-	struct htc_packet *packet;
+	struct htc_packet *packet = NULL;
 
 	ath6kl_dbg(ATH6KL_DBG_HTC,
 		   "%s: queue: 0x%lX, pkts %d\n", __func__,